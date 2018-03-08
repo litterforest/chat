fd7e07e2e36c8833f9a6f3ec6bb8bcedcc58602b
gensen <18027041863@163.com>
2018-03-08 21:57:29 +0800

    增加群发功能

--- src/main/java/com/cobee/chat/websocket/ChatServer.java
+++ src/main/java/com/cobee/chat/websocket/ChatServer.java
@@ -1,6 +1,7 @@
 package com.cobee.chat.websocket;
 
 import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
@@ -12,17 +13,20 @@ import javax.websocket.server.ServerEndpoint;
 public class ChatServer {
 	
 	private Session session;
+	private static CopyOnWriteArraySet<ChatServer> sets = new CopyOnWriteArraySet<ChatServer>();
 	
 	@OnOpen
 	public void onOpen(Session session)
 	{
 		this.session = session;
+		sets.add(this);
 		System.out.println("已建立socket连接, sessionID:" + session.getId());
 	}
 	
 	@OnClose
 	public void onClose()
 	{
+		sets.remove(this);
 		System.out.println("已关闭socket连接");
 	}
 	
@@ -31,7 +35,10 @@ public class ChatServer {
 	{
 		System.out.println(msg);
 		try {
-			this.session.getBasicRemote().sendText(msg);
+			for (ChatServer cs : sets)
+			{
+				cs.session.getBasicRemote().sendText(msg);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
